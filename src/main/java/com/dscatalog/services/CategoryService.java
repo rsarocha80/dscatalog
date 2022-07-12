586ef07b62c949212bd95c0269d86f89dd863d3c
Rodrigo Rocha <rodrigosantosrocha80@gmail.com>
2022-07-12 15:50:28 -0300

    inserting dto

--- src/main/java/com/dscatalog/services/CategoryService.java
+++ src/main/java/com/dscatalog/services/CategoryService.java
@@ -1,23 +1,37 @@
 package com.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.dscatalog.dto.CategoryDTO;
 import com.dscatalog.entities.Category;
 import com.dscatalog.repositories.CategoryRepository;
 
 @Service
 public class CategoryService {
-	
+
 	@Autowired
 	private CategoryRepository repository;
-	
+
 	@Transactional(readOnly = true)
-	public List<Category> findAll(){
-		return repository.findAll();
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		return list.stream().map(this::convereterEntidadeParaDTO).collect(Collectors.toList());
+	}
+
+	// conversores
+
+	public CategoryDTO convereterEntidadeParaDTO(Category entity) {
+
+		CategoryDTO dto = new CategoryDTO();
+		dto.setId(entity.getId());
+		dto.setName(entity.getName());
+		return dto;
+
 	}
 
 }
